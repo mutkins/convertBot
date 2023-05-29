a110336f7ed2e20f6f51893458f77948cff0ca61
Mikhail Utkin <mihut@rambler.ru>
2023-05-29 13:10:53 +0500

    fix some bugs

--- handlers/common.py
+++ handlers/common.py
@@ -9,8 +9,7 @@ import logging
 import io
 from converters import do_convert
 from tools import download_file, UploadFile
-
-
+from datetime import datetime
 
 # Configure logging
 logging.basicConfig(filename="main.log", level=logging.INFO, filemode="w",
@@ -18,23 +17,35 @@ logging.basicConfig(filename="main.log", level=logging.INFO, filemode="w",
 log = logging.getLogger("main")
 input_temp_f = os.environ.get('input_temp')
 output_temp_f = os.environ.get('output_temp')
+
+
 class MyFSM(StatesGroup):
     waiting_file = State()
     waiting_format = State()
-    dish_type = State()
-    dish_type_query = State()
 
 
-# @dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message, state: FSMContext):
+async def ask_file(message: types.Message, state: FSMContext):
     await message.answer("Отправьте файл для конвертации", parse_mode="HTML")
     await MyFSM.waiting_file.set()
 
 
-async def ask_format(message: types.Message, state: FSMContext):
+async def taking_file(message: types.Message, state: FSMContext):
+    # print(f'{datetime.now()} пришел файл {message.document.file_name}')
+
+    if message.content_type == 'document':
+        await download_img(message, state)
+    else:
+        await send_err_incorrect_frmt(message, state)
+
+
+async def download_img(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
-        data['input_filepath'] = await download_file(bot=bot, file_id=message.document.file_id, file_name=message.document.file_name)
-        print(data['input_filepath'])
+        data['input_filepath'] = await download_file(bot=bot, file_id=message.document.file_id,
+                                                     file_name=message.document.file_name)
+    await ask_format(message, state)
+
+
+async def ask_format(message: types.Message, state: FSMContext):
     await message.answer("Выберите целевой формат", reply_markup=keyboards.get_formats_kb())
     await MyFSM.waiting_format.set()
 
@@ -45,19 +56,11 @@ async def return_converted_file(message: types.Message, state: FSMContext):
             output_filepath = do_convert(input_filepath=data['input_filepath'], target_format=message.text)
         except Exception as e:
             await message.answer(e)
-            await cancel_handler(message=message,state=state)
+            await cancel_handler(message=message, state=state)
             raise Exception
     await state.finish()
     with UploadFile(output_filepath) as file:
         await message.answer_document(file)
-
-
-async def taking_file(message: types.Message, state: FSMContext):
-    a = message.content_type
-    if message.content_type == 'document':
-        await ask_format(message, state)
-    else:
-        await send_err_incorrect_frmt(message, state)
 
 
 async def send_err_incorrect_frmt(message: types.Message, state: FSMContext):
@@ -74,14 +77,19 @@ async def cancel_handler(message: types.Message, state: FSMContext):
     if current_state:
         await state.finish()
     # Cancel state and inform user about it
-    await send_welcome(message)
+    await send_welcome(message, state)
 
+
+async def send_welcome(message: types.Message, state: FSMContext):
+    await message.answer(text='Бот предназначен для конвертации файлов из одного формата в другой.\n'
+                       'Пришлите /convert для начала')
 
 def register_handlers(dp: Dispatcher):
     # A1 user sends /help or smthg like it
+    dp.register_message_handler(cancel_handler, state='*', commands='отмена')
     dp.register_message_handler(send_welcome, commands=['start', 'help', 'хелп'])
+    dp.register_message_handler(ask_file, commands='convert')
     dp.register_message_handler(taking_file, state=MyFSM.waiting_file, content_types='any')
     dp.register_message_handler(return_converted_file, state=MyFSM.waiting_format)
-    dp.register_message_handler(cancel_handler, state='*', commands='отмена')
 
 
