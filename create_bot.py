4a5588229767525e43a122f352d8f417e2d8ef9b
Mikhail Utkin <mihut@rambler.ru>
2023-08-25 11:40:15 +0500

    move to local server

--- create_bot.py
+++ create_bot.py
@@ -1,11 +1,14 @@
 import os
 from aiogram import Bot, Dispatcher, types
+from aiogram.bot.api import TelegramAPIServer
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from dotenv import load_dotenv
 
 storage = MemoryStorage()
 load_dotenv()
 
+
+local_server = TelegramAPIServer.from_base('http://localhost:8008')
 # Initialize bot and dispatcher
-bot = Bot(token=os.environ.get('tgBot_id'))
+bot = Bot(token=os.environ.get('tgBot_id'), server=local_server)
 dp = Dispatcher(bot, storage=storage)
