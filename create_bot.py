48e2f6d26d3a71df6bd439d0b9aaa4ea667736f0
Mikhail Utkin <mihut@rambler.ru>
2023-08-25 17:49:57 +0500

    move back to tg server

--- create_bot.py
+++ create_bot.py
@@ -8,7 +8,8 @@ storage = MemoryStorage()
 load_dotenv()
 
 
-local_server = TelegramAPIServer.from_base('http://localhost:8008')
+# local_server = TelegramAPIServer.from_base('http://192.168.0.108:8008')
 # Initialize bot and dispatcher
-bot = Bot(token=os.environ.get('tgBot_id'), server=local_server)
+# bot = Bot(token=os.environ.get('tgBot_id'), server=local_server)
+bot = Bot(token=os.environ.get('tgBot_id'))
 dp = Dispatcher(bot, storage=storage)
